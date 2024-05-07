8f5c8ce1bd5a98a8a02d93783fb59d41114d5ebd
Cintra <gui7lk@gmail.com>
2024-05-07 14:59:26 -0300

    refact: fetch url

--- src/components/infra/fetch-logic/fetchUrl.ts
+++ src/components/infra/fetch-logic/fetchUrl.ts
@@ -1,14 +1,19 @@
-export async function fetchUrl(url: string, options?: RequestInit) {
+export async function fetchUrl<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
     cache: "force-cache",
     ...options,
   });
 
   if (!response.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error(
+      `Failed to fetch data from ${url}. Status: ${response.status}`
+    );
   }
 
   return response.json();
 }
 
-export const revalidate = 1000 * 60 * 60 * 24;
+export const revalidate = 1000 * 60 * 60 * 24; // 24 hours
